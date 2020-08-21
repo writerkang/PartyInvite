a4d03ae08afd88d0799f89b697591853d93c60c4
writerkang <goodwriterkang@gmail.com>
2020-08-21 22:00:18 +0900

    end Chapter2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvite.Models;
 
 namespace PartyInvite.Controllers
 {
@@ -16,9 +17,26 @@ namespace PartyInvite.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+            // TODO: 파티 주최자에게 전자 메일로 응답한다.
+            return View("Thanks", guestResponse);
+
+            }
+            else
+            {
+                //유효성 검사 오류가 존재한다.
+                return View();
+            }
         }
     }
 }
--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvite.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Plese enter a valid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
         public bool? WillAttend { get; set; }
 
     }
