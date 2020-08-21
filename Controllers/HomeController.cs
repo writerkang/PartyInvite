069041bf94fcffb6ddefb525c1fe49ca116fa3ed
writerkang <goodwriterkang@gmail.com>
2020-08-21 14:37:30 +0900

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Mornig" : "Good Afternoon";
+            return View();
+        }
+    }
+}
